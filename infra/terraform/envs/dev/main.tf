e9f056b9df50416be3b13ff59c6d8f3921c638da
Ayanle Mohamed <Ayanle_150@hotmail.com>
2026-01-21 19:41:56 +0100

    infra(terraform): add AKS outputs for dev

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -15,3 +15,32 @@ resource "azurerm_container_registry" "p2_acr" {
   sku                 = "Basic"
   admin_enabled       = false
 }
+
+resource "azurerm_kubernetes_cluster" "p2_aks" {
+  name                = "aks-p2-dev"
+  location            = azurerm_resource_group.p2_dev.location
+  resource_group_name = azurerm_resource_group.p2_dev.name
+  dns_prefix          = "p2dev"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+
+    # Azure for Students (norwayeast) allows v2, not Standard_B2s
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.p2_acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.p2_aks.kubelet_identity[0].object_id
+}
